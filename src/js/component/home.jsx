011b64c945c5e857931d41a01bf1164834c81544
Nicolettastr <nicolettastruggia@hotmail.com>
2022-12-18 01:00:39 +0000

    third section decoration with buttons

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,17 +7,22 @@ import BigCloud from "./bigCloud.jsx";
 import Sun from "./sun.jsx";
 import Info from "./info.jsx";
 import LittleCloud from "./littleCloud.jsx";
+import MediumCloud from "./mediumCloud.jsx";
+import FarmImages from "./farmImages.jsx";
 
 //IMAGES
 import koala from "../../img/personajes/koala.png";
-import sol from "../../img/personajes/sol.png"
+import sol from "../../img/personajes/sol.png";
+import vaca from "../../img/personajes/vaca2.png";
+import girasolBtn from "../../img/personajes/girasolBtn.png";
 
 //JSON files
 import dataCloud from "../dataCloud.json"
 import dataNav from "../dataNav.json";
 import dataLittleCloud from "../dataLittleCloud.json"
 import dataInfo from "../dataInfo.json"
-import MediumCloud from "./mediumCloud.jsx";
+import dataFarm from "../dataFarm.json"
+import dataBoton from "../dataBoton.json"
 
 //create your first component
 const Home = () => {
@@ -38,6 +43,9 @@ const Home = () => {
       <LittleCloud class="place" dataLittleCloud={dataLittleCloud}/>
       <Info dataInfo={dataInfo}/>
 		</section>
+    <section className="lets-go-section">
+      <FarmImages dataFarm={dataFarm} vaca={vaca} girasolBtn={girasolBtn} boton={dataBoton}/>
+    </section>
 	  </main>
     </>
   );
